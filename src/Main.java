fb9a09d20c35d9b125d1f8e46f9fd4344197d17a
PaulSPuntusov <pps_ru@mail.ru>
2024-09-18 22:34:01 +0300

    сделал

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int n = 20;
+        int s;
+        ArrayList<Integer> first = new ArrayList(n);
+        for (int i = 0; i < n ; i ++) {
+            first.add(i, i);
+            System.out.print(first.get(i)+" ");
+        }
+        System.out.println("");
+        for (int i = 0; i < n - 1; i = i + 2) {
+            s = first.get(i);
+            first.set(i, first.get(i + 1));
+            first.set(i+1, s);
+            System.out.print(first.get(i)+" ");
+            System.out.print(first.get(i+1)+" ");
+        }
     }
 }
