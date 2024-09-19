bf8002187e43489a55fddb7b91897184fe7aaff8
PaulSPuntusov <pps_ru@mail.ru>
2024-09-19 20:30:18 +0300

    сделал

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,49 @@
 import java.util.ArrayList;
 
 public class Main {
+    static int n = 20;
+    static int[] intArray = new int[n];
     public static void main(String[] args) {
-        int n = 20;
-        int s;
         ArrayList<Integer> intList = new ArrayList(n);
         for (int i = 0; i < n ; i ++) {
             intList.add(i, i);
             System.out.print(intList.get(i)+" ");
         }
         System.out.println("");
+        /*
         public static void bubbleSort(ArrayList intList){
-            for(int i = 0; i < intList.size() - i; i++){
-                for(int j = 0; j < intList.get(i) - i - 1; j++){
+        for(int i = 0; i < intList.size() - i; i++){
+            for(int j = 0; j < intList.get(i) - i - 1; j++){
+                if(intList.get(j) > intList.get(j + 1)){
                     int temp = intList.get(j);
                     intList.set(j,intList.get(j+1));
                     intList.set(j+1,temp);
                 }
-                System.out.print(intList.get(i)+" ");
+
             }
         }
     }
+
+         */
+        //Integer[] intArray = intList.toArray(new Integer[0]);
+
+        for(int i = 0; i < intArray.length; i++){
+            intArray[i] = intList.get(i);
+            System.out.println(intArray[i]);
+        }
+        bubbleSort(intArray);
+    }
+    public static void bubbleSort(int[] intArray){
+        for(int i = 0; i < intArray.length - 1; i++){
+            for(int j = 0; j < intArray.length - i - 1; j++){
+                if(intArray[j] > intArray[j + 1]){
+                    int temp = intArray[j];
+                    intArray[j] = intArray[j+1];
+                    intArray[j+1] = temp;
+                }
+            }
+            System.out.print(intArray[i]+" ");
+        }
+    }
+
 }
